1670c3d6d0e56514801a9d3ccfafd16c785c2ebf
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-10-09 21:22:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Linked List Matrix/linked-list-matrix.py
+++ Difficulty: Easy/Linked List Matrix/linked-list-matrix.py
@@ -12,16 +12,31 @@ class Node():
 
 class Solution:
     def constructLinkedMatrix(self, mat):
-        #your code goes here
-        n,m = len(mat), len(mat[0])
-        def fu(i,j):
-            if(i>=n or j>=m):
-                return
-            temp = Node(mat[i][j])
-            temp.right = fu(i,j+1)
-            temp.down = fu(i+1,j)
-            return temp;
-        return fu(0,0)
+        #your code goes 
+        n=len(mat)
+        prev_row_head=None
+        curr_row_head=None
+        prev=None
+        head=None
+        for i in range(n):
+            for j in range(n):
+                if j==0:
+                    if i==0:
+                        head=Node(mat[i][j])
+                        prev=head
+                    else:
+                        prev=Node(mat[i][j])
+                    curr_row_head=prev
+                else:
+                    prev.right=Node(mat[i][j])
+                    prev=prev.right
+                if i!=0:
+                    prev_row_head.down=prev
+                    prev_row_head=prev_row_head.right
+                if j==n-1:
+                    prev_row_head=curr_row_head
+        return head
+            
 
 
 #{ 
