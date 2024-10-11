4210e7ae5308f64489e20bfb3265313a7b5eb1d7
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-10-11 12:35:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reorganize The Array/reorganize-the-array.py
@@ -0,0 +1,32 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
+class Solution:
+    def rearrange(self, arr):
+        #Code here
+        d = {}
+        l =[]
+        for i in arr:
+            d[i] = d.get(i, 0)+1
+        for i in range(len(arr)):
+            if i in d:
+                l.append(i)
+            else:
+                l.append(-1)
+        return l
+
+#{ 
+ # Driver Code Starts.
+def main():
+    t = int(input())
+    for _ in range(t):
+        input_str = input()
+        arr = list(map(int, input_str.split()))
+        solution = Solution()
+        ans = solution.rearrange(arr)
+        print(" ".join(map(str, ans)))
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
