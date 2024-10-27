a9b6a055ecfd3d2948d273b7d2bd63256b000b78
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-10-27 10:57:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Triplet Family/triplet-family.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findTriplet(self, arr):
+        n = len(arr)
+        vis = set(arr)
+        for i in range(n):
+            for j in range(i+1,n):
+                if arr[i] + arr[j] in vis:
+                    return True
+        return False
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+# Position this line where user code will be pasted.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.findTriplet(arr)
+        if (res):
+            print("true")
+        else:
+            print("false")
+        # print(res)
+        print("~")
+        t -= 1
+
+# } Driver Code Ends
