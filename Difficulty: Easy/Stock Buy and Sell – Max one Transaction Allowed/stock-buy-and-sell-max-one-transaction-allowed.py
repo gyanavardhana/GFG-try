2df93ff5480ef5d9e93d5a98a4d890819f5ea564
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-11-22 19:22:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumProfit(self, prices):
+        # code here
+        mp = 0
+        mbp = prices[0]
+        for i in range(1,len(prices)):
+            mbp = min(mbp, prices[i])
+            mp = max(mp, prices[i]-mbp)
+        return mp
+        
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())  # Read number of test cases
+    for _ in range(t):
+        # Read input and split it into a list of integers
+        prices = list(map(int, input().split()))
+        # Create a Solution object and calculate the result
+        obj = Solution()
+        result = obj.maximumProfit(prices)
+        # Print the result
+        print(result)
+
+# } Driver Code Ends
