49c656e936b656e9c69e93c2656cb6b29d4322ff
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-11-23 19:45:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+    def getMinDiff(self,k, arr):
+        # code here
+        n = len(arr)
+        arr.sort()
+        ans = arr[n-1] - arr[0]
+        for i in range(1,n):
+            mini = min(arr[0]+k, arr[i]-k)
+            maxi = max(arr[i-1]+k, arr[n-1]-k)
+            ans = min(ans, maxi- mini)
+        return ans
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        k = int(input())
+        arr = list(map(int, input().strip().split()))
+        solution = Solution()
+        res = solution.getMinDiff(k, arr)
+        print(res)
+        print("~")
+
+# } Driver Code Ends
